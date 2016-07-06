0baa19ef6011c317e86e5dd2690708e460e571cc
Greg Burkett <greg@ripfish.com>
2016-07-05 23:17:04 -0400

    Hubspot LP start

---
+++ base-template-hubspot-blog-lp.php
@@ -0,0 +1,232 @@
+<?php get_template_part('templates/head'); ?>
+
+<body <?php body_class(); ?> id="body_new2016theme">
+
+  <!--[if lt IE 8]><div class="alert alert-warning"><?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?></div><![endif]-->
+
+  <?php
+    do_action('get_header');
+    // Use Bootstrap's navbar if enabled in config.php
+    if (current_theme_supports('bootstrap-top-navbar')) {
+      get_template_part('templates/header-top-navbar');
+    } else {
+      get_template_part('templates/header');
+    }
+  ?>
+  <div id="main_wrap">
+    <div class="call-to-action-box ctab-less-padding" style="background-image:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/hubspot/blogheader.png')">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-10 col-xs-12 col-sm-offset-1">
+            <div class="row">
+              <div id="blog_header_hero" class="col-sm-12 text-center even_more_bottom_margin more_top_margin">
+                <h1>Protect, Grow, and Act on What Matters.</h2>
+                <p class="lead">Stay up-to-date with the latest out of Envysion</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="wrap blog_verticals_column_wrap">
+    <div class="container">
+      <div class="col-sm-12 col-centered">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="verticals_wrap">
+              <a class="verticals_header_button" href="#">Quick Service Restaurants</a>
+              <div class="verticals_posts_wrap">
+                <div class="subscribe_vertical_box">
+                  <input type="text" placeholder="Enter your email to subscribe...">
+                </div>
+                <ul class="list-unstyled verticals_posts_ul">
+                  <li>
+                    <a href="#">
+                      <img src="http://placehold.it/365x176">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                </ul>
+                <a href="#" class="vertical_col_footer_readmore">Read more</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="verticals_wrap">
+              <a class="verticals_header_button" href="#">Wireless</a>
+              <div class="verticals_posts_wrap">
+                <div class="subscribe_vertical_box">
+                  <input type="text" placeholder="Enter your email to subscribe...">
+                </div>
+                <ul class="list-unstyled verticals_posts_ul">
+                  <li>
+                    <a href="#">
+                      <img src="http://placehold.it/365x176">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                </ul>
+                <a href="#" class="vertical_col_footer_readmore">Read more</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="verticals_wrap">
+              <a class="verticals_header_button" href="#">Retail</a>
+              <div class="verticals_posts_wrap">
+                <div class="subscribe_vertical_box">
+                  <input type="text" placeholder="Enter your email to subscribe...">
+                </div>
+                <ul class="list-unstyled verticals_posts_ul">
+                  <li>
+                    <a href="#">
+                      <img src="http://placehold.it/365x176">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div class="post_blurb_wrap">
+                        <h4>Quick Service Restaurants see shift in consumer tastes, wait times.</h4>
+                        <p class="excerpt">While leading the pack for the last half decade, QSRs are starting to see a shift in consumer tastes, yet the...</p>
+                      </div>
+                    </a>
+                  </li>
+                </ul>
+                <a href="#" class="vertical_col_footer_readmore">Read more</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php get_template_part('templates/footer'); ?>
+  </div>
+</body>
+</html>
