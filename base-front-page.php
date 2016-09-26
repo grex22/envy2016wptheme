7f8ede441b11e82452b36ff0af821e7e0b985702
Greg Burkett <greg@ripfish.com>
2016-09-26 12:18:31 -0400

    Adding in pip that i forgot!

--- base-front-page.php
+++ base-front-page.php
@@ -19,6 +19,7 @@
         <li data-target="#home_carousel" data-slide-to="0" class="active"></li>
         <li data-target="#home_carousel" data-slide-to="1"></li>
         <li data-target="#home_carousel" data-slide-to="2"></li>
+        <li data-target="#home_carousel" data-slide-to="3"></li>
       </ol>
 
       <!-- Wrapper for slides -->
